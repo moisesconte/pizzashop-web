7c2a627b268481b2d60cf49b10c6e3414d4deefc
Moisés Conte <mtfc.dk@hotmail.com>
2024-01-15 23:40:34 -0300

    master

---
+++ src/pages/routes.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './_layouts/app'
+import { AuthLayout } from './_layouts/auth'
+import { Dashboard } from './app/dashboard'
+import { SignIn } from './auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
+  // { path: '/', element: <Dashboard /> },
+  // { path: '/sign-in', element: <SignIn /> },
+])
